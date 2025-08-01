f1e19bc22510e4dd6d80671c05bfde70bdc1f466
Zahid Hasan <mrzahidxy.com>
2025-08-01 12:25:07 +0600

    check

--- src/app/(root)/(home)/MainBanner.component.tsx
+++ src/app/(root)/(home)/MainBanner.component.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
+
+
 
 export const MainBanner = () => {
+
+useEffect(() => {
+  console.log("api URL is", process.env.NEXT_PUBLIC_BASE_URL);
+}, []);
+
   return (
     <div className="h-96 relative my-4">
       <Image
